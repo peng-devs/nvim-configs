a2856fea159ea54bb9ec0863617c3b4c31d3e721
peng <peng.devs@gmail.com>
2025-06-12 19:35:58 +0800

    feat: add colorscheme and statusline plugins
    - Add gruvbox-material colorscheme with transparency support
    - Add lualine statusline with custom gruvbox-material theme
    - Update lazy-lock.json with new plugin dependencies

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'gruvbox-material',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function() return ' ' end,
+            padding = 0,  -- 調整寬度
+          }
+        },
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {'filename'},
+        lualine_x = {'diagnostics'},
+        lualine_y = {'filetype'},
+        lualine_z = {'progress'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+    })
+  end,
+}
