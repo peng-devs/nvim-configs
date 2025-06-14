fbacb3dcb8a274b0f2567c42c95d2c0de8e94816
peng <peng.devs@gmail.com>
2025-06-15 00:04:05 +0800

    feat: update plugin configurations
    - Add toggleterm.nvim plugin for terminal management
    - Enhance colorscheme transparency setting
    - Simplify telescope search keymap

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.g.gruvbox_material_transparent_background = 1
+		vim.g.gruvbox_material_transparent_background = 2
 		vim.g.gruvbox_material_enable_italic = 1
 		vim.g.gruvbox_material_enable_bold = 1
 		vim.cmd("colorscheme gruvbox-material")
